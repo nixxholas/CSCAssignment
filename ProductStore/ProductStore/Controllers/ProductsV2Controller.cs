f6ddf97f7138cf5b3ecec78c03bf61e450690c2d
nixholas <nixholas@outlook.com>
2017-06-19 22:00:12 +0800

    Source: ProductStore => End of Practical 2
    Signed-off-by: nixholas <nixholas@outlook.com>

---
+++ ProductStore/ProductStore/Controllers/ProductsV2Controller.cs
@@ -0,0 +1,103 @@
+﻿using ProductStore.Models;
+using ProductStore.Repositories;
+using ProductStore.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace ProductStore.Controllers
+{
+    [Route("api/v2/products")]
+    public class ProductsV2Controller : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        //code for version 1
+        [HttpGet]
+        public string[] GetVersion()
+        {
+            return new string[] { "hello", "version 2", "2" };
+        }
+
+        [HttpGet]
+        //http://localhost:9000/api/v1/products/message?name1=ji&name2=jii1&name3=ji3
+        public HttpResponseMessage GetMultipleNames(String name1, string name2, string name3)
+        {
+            var response = new HttpResponseMessage();
+            response.Content = new StringContent("<html><body>Hello World " +
+            " name1 =" + name1 +
+            " name2= " + name2 +
+            " name3=" + name3 +
+            " is provided in path parameter</body></html>");
+
+            response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+            return response;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll().ToList();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = repository.Get(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public Product PostProductDeprecated(Product item)
+        {
+            item = repository.Add(item);
+
+            return item;
+        }
+
+        [HttpPost]
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        [HttpPut]
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        [HttpPut]
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(id);
+        }
+    }
+}
---
+++ ProductStore/ProductStore/Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductStore.Repositories.Interfaces
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAll();
+        Product Get(int id);
+        Product Add(Product item);
+        void Remove(int id);
+        bool Update(Product item);
+    }
+}
