a2965192ff9d495db6f27a88329ac2114de16f79
nixholas <nixholas@outlook.com>
2017-06-20 01:40:14 +0800

    Source: ProductsV3Controller: Basic Implementation
    Signed-off-by: nixholas <nixholas@outlook.com>

--- ProductStore/ProductStore/Models/Product.cs
+++ ProductStore/ProductStore/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,11 @@ namespace ProductStore.Models
     public class Product
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Category { get; set; }
+        [Required]
+        [Range(0, 100)]
         public decimal Price { get; set; }
     }
 }
